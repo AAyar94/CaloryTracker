304fc3181e4d8b908ab5f90ffd1cd14c43d95afa
Adem Ayar <ademayar09@gmail.com>
2024-01-12 02:57:17 +0300

    NutrientGoalScreen impl

---
+++ onboarding/onboarding_domain/src/main/java/com/aayar94/onboarding_domain/di/OnboardingDomainModule.kt
@@ -0,0 +1,21 @@
+package com.aayar94.onboarding_domain.di
+
+import com.aayar94.onboarding_domain.use_case.ValidateNutrients
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.android.components.ViewModelComponent
+import dagger.hilt.android.scopes.ViewModelScoped
+import dagger.hilt.components.SingletonComponent
+
+@Module
+@InstallIn(ViewModelComponent::class)
+object OnboardingDomainModule {
+
+    @Provides
+    @ViewModelScoped
+    fun provideValidateNutrientsUseCase(): ValidateNutrients {
+        return ValidateNutrients()
+    }
+
+}
---
+++ onboarding/onboarding_presentation/src/main/java/com/aayar94/onboard_presentation/nutrient_goal/NutrientGoalEvent.kt
@@ -0,0 +1,10 @@
+package com.aayar94.onboard_presentation.nutrient_goal
+
+sealed class NutrientGoalEvent {
+
+    data class OnCarbRatioEnter(val ratio: String) : NutrientGoalEvent()
+    data class OnProteinRatioEnter(val ratio: String) : NutrientGoalEvent()
+    data class OnFatRatioEnter(val ratio: String) : NutrientGoalEvent()
+    object OnNextClick : NutrientGoalEvent()
+
+}
