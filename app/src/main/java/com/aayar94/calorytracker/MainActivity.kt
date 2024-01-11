422b84616189d72b33c25cf308545e0c98ff3f29
Adem Ayar <ademayar09@gmail.com>
2024-01-12 02:06:19 +0300

    Age Screen impl

--- app/src/main/java/com/aayar94/calorytracker/MainActivity.kt
+++ app/src/main/java/com/aayar94/calorytracker/MainActivity.kt
@@ -3,12 +3,18 @@ package com.aayar94.calorytracker
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.Scaffold
+import androidx.compose.material.rememberScaffoldState
+import androidx.compose.ui.Modifier
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
 import com.aaayar94.core.navigation.Route
 import com.aayar94.calorytracker.navigation.navigate
 import com.aayar94.calorytracker.ui.theme.CaloryTrackerTheme
+import com.aayar94.onboard_presentation.age.AgeScreen
 import com.aayar94.onboard_presentation.gender.GenderScreen
 import com.aayar94.onboard_presentation.welcome.WelcomeScreen
 import dagger.hilt.android.AndroidEntryPoint
@@ -20,40 +26,51 @@ class MainActivity : ComponentActivity() {
         setContent {
             CaloryTrackerTheme {
                 val navController = rememberNavController()
-                NavHost(navController = navController, startDestination = Route.WELCOME) {
-                    composable(Route.WELCOME) {
-                        WelcomeScreen(onNavigate = navController::navigate)
-                    }
-                    composable(Route.AGE) {
+                val scaffoldState = rememberScaffoldState()
+                Scaffold(modifier = Modifier.fillMaxSize(), scaffoldState = scaffoldState) {
+                    NavHost(
+                        navController = navController,
+                        startDestination = Route.WELCOME,
+                        modifier = Modifier.padding(it)
+                    ) {
+                        composable(Route.WELCOME) {
+                            WelcomeScreen(onNavigate = navController::navigate)
+                        }
+                        composable(Route.AGE) {
+                            AgeScreen(
+                                scaffoldState = scaffoldState,
+                                onNavigate = navController::navigate
+                            )
+                        }
+                        composable(Route.GENDER) {
+                            GenderScreen(onNavigate = navController::navigate)
+                        }
+                        composable(Route.HEIGHT) {
+
+                        }
+                        composable(Route.WEIGHT) {
+
+                        }
+                        composable(Route.NUTRIENT_GOAL) {
+
+                        }
+                        composable(Route.ACTIVITY) {
+
+                        }
+                        composable(Route.GOAL) {
+
+                        }
+                        composable(Route.TRACKER_OVERVIEW) {
+
+                        }
+                        composable(Route.SEARCH) {
+
+                        }
 
                     }
-                    composable(Route.GENDER) {
-                        GenderScreen(onNavigate = navController::navigate)
-                    }
-                    composable(Route.HEIGHT) {
 
-                    }
-                    composable(Route.WEIGHT) {
-
-                    }
-                    composable(Route.NUTRIENT_GOAL) {
-
-                    }
-                    composable(Route.ACTIVITY) {
-
-                    }
-                    composable(Route.GOAL) {
-
-                    }
-                    composable(Route.TRACKER_OVERVIEW) {
-
-                    }
-                    composable(Route.SEARCH) {
-
-                    }
 
                 }
-
             }
         }
     }
---
+++ onboarding/onboarding_presentation/src/main/java/com/aayar94/onboard_presentation/age/AgeViewModel.kt
@@ -0,0 +1,52 @@
+package com.aayar94.onboard_presentation.age
+
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.aaayar94.core.domain.preferences.Preferences
+import com.aaayar94.core.domain.use_case.FilterOutDigits
+import com.aaayar94.core.navigation.Route
+import com.aaayar94.core.util.UiEvent
+import com.aaayar94.core.util.UiText
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.channels.Channel
+import kotlinx.coroutines.flow.receiveAsFlow
+import kotlinx.coroutines.launch
+import javax.inject.Inject
+
+@HiltViewModel
+class AgeViewModel @Inject constructor(
+    private val preferences: Preferences,
+    private val filterOutDigits: FilterOutDigits
+) : ViewModel() {
+
+    var age by mutableStateOf("20")
+        private set
+
+    private val _uiEvent = Channel<UiEvent>()
+    val uiEvent = _uiEvent.receiveAsFlow()
+
+    fun onAgeEnter(age: String) {
+        if (age.length <= 3) {
+            this.age = filterOutDigits(age)
+        }
+    }
+
+    fun onNextClick() {
+        viewModelScope.launch {
+            val ageNumber = age.toIntOrNull() ?: kotlin.run {
+                _uiEvent.send(
+                    UiEvent.ShowSnackbar(
+                        UiText.StringResource(com.aayar94.core.R.string.error_age_cant_be_empty)
+                    )
+                )
+                return@launch
+            }
+            preferences.saveAge(ageNumber)
+            _uiEvent.send(UiEvent.Navigate(Route.HEIGHT))
+        }
+
+    }
+}
