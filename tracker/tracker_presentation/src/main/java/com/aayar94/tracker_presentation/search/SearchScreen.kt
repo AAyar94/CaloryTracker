cb649c809be52158fe88c19566787cb190a2ee69
Adem Ayar <ademayar09@gmail.com>
2024-01-14 00:05:34 +0300

    Add search response view, SearchScreen edit

--- tracker/tracker_presentation/src/main/java/com/aayar94/tracker_presentation/search/SearchScreen.kt
+++ tracker/tracker_presentation/src/main/java/com/aayar94/tracker_presentation/search/SearchScreen.kt
@@ -1,24 +1,34 @@
 package com.aayar94.tracker_presentation.search
 
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material.CircularProgressIndicator
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.ScaffoldState
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.platform.LocalSoftwareKeyboardController
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.style.TextAlign
 import androidx.hilt.navigation.compose.hiltViewModel
 import com.aaayar94.core.util.UiEvent
 import com.aayar94.core_ui.LocalSpacing
+import com.aayar94.tracker_domain.model.MealType
 import com.aayar94.tracker_presentation.search.components.SearchTextField
+import com.aayar94.tracker_presentation.search.components.TrackableFoodItem
+import java.time.LocalDate
 import com.aayar94.core.R.string as AppText
 
 @OptIn(ExperimentalComposeUiApi::class)
@@ -51,7 +61,7 @@ fun SearchScreen(
                 }
 
                 is UiEvent.NavigateUp -> {
-                    onNavigateUp
+                    onNavigateUp()
                 }
 
                 else -> {
@@ -76,6 +86,52 @@ fun SearchScreen(
             onSearch = { viewModel.onEvent(SearchEvent.OnSearch) },
             onFocusChanged = {
                 viewModel.onEvent(SearchEvent.OnSearchFocusChange(it.isFocused))
-            })
+            }
+        )
+        Spacer(modifier = Modifier.height(spacing.spaceMedium))
+        LazyColumn(modifier = Modifier.fillMaxSize()) {
+            items(state.trackableFood) { food ->
+                TrackableFoodItem(
+                    trackableFoodUiState = food,
+                    onClick = {
+                        viewModel.onEvent(
+                            SearchEvent.OnToggleTrackableFood(food = food.food)
+                        )
+                    },
+                    onAmountChanged = {
+                        viewModel.onEvent(
+                            SearchEvent.OnAmountForFoodChange(
+                                food = food.food,
+                                amount = it
+                            )
+                        )
+                    },
+                    onTrack = {
+                        viewModel.onEvent(
+                            SearchEvent.OnTrackFoodClick(
+                                food = food.food,
+                                mealType = MealType.fromString(mealName),
+                                date = LocalDate.of(year, month, dayOfMonth)
+                            )
+                        )
+                    },
+                    modifier = Modifier.fillMaxWidth()
+                )
+            }
+        }
+    }
+    Box(modifier = Modifier.fillMaxSize(), contentAlignment = Alignment.Center) {
+        when {
+            state.isSearching -> CircularProgressIndicator()
+            state.trackableFood.isEmpty() -> {
+                Text(
+                    text = stringResource(id = AppText.no_results),
+                    style = MaterialTheme.typography.body1,
+                    textAlign = TextAlign.Center
+                )
+            }
+        }
+
+
     }
 }
