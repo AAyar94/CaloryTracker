aabe4a5f7e7e57205faa15b235c5253b35ffcb8b
Adem Ayar <ademayar09@gmail.com>
2024-01-13 14:05:40 +0300

    create day selector composable

--- tracker/tracker_presentation/src/main/java/com/aayar94/tracker_presentation/tracker_overview/TrackerOverviewScreen.kt
+++ tracker/tracker_presentation/src/main/java/com/aayar94/tracker_presentation/tracker_overview/TrackerOverviewScreen.kt
@@ -1,6 +1,9 @@
 package com.aayar94.tracker_presentation.tracker_overview
 
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.runtime.Composable
@@ -9,6 +12,7 @@ import androidx.compose.ui.platform.LocalContext
 import androidx.hilt.navigation.compose.hiltViewModel
 import com.aaayar94.core.util.UiEvent
 import com.aayar94.core_ui.LocalSpacing
+import com.aayar94.tracker_presentation.tracker_overview.components.DaySelector
 import com.aayar94.tracker_presentation.tracker_overview.components.NutrientsHeader
 
 @Composable
@@ -26,7 +30,19 @@ fun TrackerOverviewScreen(
             .fillMaxSize()
             .padding(bottom = spacing.spaceMedium)
     ) {
-        item { NutrientsHeader(state = state) }
+        item {
+            NutrientsHeader(state = state)
+            Spacer(modifier = Modifier.height(spacing.spaceMedium))
+            DaySelector(
+                date = state.date,
+                onPreviousDayClick = { viewModel.onEvent(TrackerOverviewEvent.OnPreviousDayClick) },
+                onNextDayClick = { viewModel.onEvent(TrackerOverviewEvent.OnNextDayClick) },
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(horizontal = spacing.spaceMedium)
+            )
+            Spacer(modifier = Modifier.height(spacing.spaceMedium))
+        }
     }
 
 }
